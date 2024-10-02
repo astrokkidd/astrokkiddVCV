21c5ed5e1a3ad1b4fd47368310629f1e8168dfe5
Josiah Parrott <josiahparrott@gmail.com>
2024-10-02 00:36:28 -0400

    Update harmosc.cpp

--- src/harmosc.cpp
+++ src/harmosc.cpp
@@ -36,6 +36,28 @@ struct Harm_osc : Module {
 
 	float phase = 0.f;
 
+	#define NUM_WAVE_SAMPLES 1000
+
+	float sineWaveTable[NUM_WAVE_SAMPLES];
+
+	float harmonicLevels[8];
+	float harmonicPhases[8] = {0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f};  // Up to 8 harmonics
+
+	float mapToTable(float phase) {
+		int tableIndex = (int)(phase * NUM_WAVE_SAMPLES);
+		tableIndex = tableIndex % NUM_WAVE_SAMPLES;
+
+		return (sineWaveTable[tableIndex]);
+	}
+
+	void initializeTable()
+	{
+		for (int i = 0; i < NUM_WAVE_SAMPLES; i++)
+		{
+			sineWaveTable[i] = 5.0f * std::sin(2.f * M_PI * ((float)i / NUM_WAVE_SAMPLES));
+		}
+	}
+
 	Harm_osc() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configParam(VCA1_PARAM, 0.f, 1.f, 0.f, "");
@@ -46,7 +68,7 @@ struct Harm_osc : Module {
 		configParam(VCA6_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(VCA7_PARAM, 0.f, 1.f, 0.f, "");
 		configParam(VCA8_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(FREQ_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(FREQ_PARAM, -5.f, 5.f, 0.f, "Freq (1V/Oct)", " Hz", 2, dsp::FREQ_C4);
 		configInput(VOCT_INPUT, "");
 		configInput(CV1_INPUT, "");
 		configInput(CV2_INPUT, "");
@@ -57,28 +79,40 @@ struct Harm_osc : Module {
 		configInput(CV7_INPUT, "");
 		configInput(CV8_INPUT, "");
 		configOutput(OUT_OUTPUT, "");
+
+		initializeTable();
 	}
 
 	void process(const ProcessArgs& args) override {
-		/*
+		
 		float baseFreq = params[FREQ_PARAM].getValue();
-		float outputSignal = 0.0f;
+		float tempOut = 0.f;
+
+		if (baseFreq < 10.f)
+				baseFreq = 10.f;
+			else if (baseFreq > 20000.f)
+				baseFreq = 20000.f;
+
+		harmonicPhases[0] += baseFreq * args.sampleTime;
+			if (harmonicPhases[0] >= 1.f)
+				harmonicPhases[0] -= 1.f;
+		
+		float outputSignal = 0.f;		
 
 		for (int i = 0; i < 8; i++) {
-				float harmFreq = baseFreq * (i + 1);
+			
+			float harmonicFreq = baseFreq * (i + 1);
 
-				float amp = params[VCA1_PARAM + i].getValue();
+			if (getInput(CV1_INPUT + i).isConnected())
+				harmonicLevels[i] = 0.1f * getInput(CV1_INPUT + i).getVoltage();
+			else
+				harmonicLevels[i] = getParam(VCA1_PARAM + i).getValue();
 
-				phase += harmFreq * args.sampleTime;
-				if (phase >= 1.f) { phase -= 1.f; }
-
-				float sine = std::sin(2.f * M_PI * phase);
-
-				float harmSignal = amp * sine;
-				outputSignal += harmSignal;
+			tempOut += mapToTable((i+1) * harmonicPhases[0]);
 		}
 
-		outputs[OUT_OUTPUT].setVoltage(outputSignal);*/
+		// Output final signal
+		outputs[OUT_OUTPUT].setVoltage(5.f * tempOut);  // Scale output to appropriate voltage range
 	}
 };
 
@@ -93,27 +127,27 @@ struct Harm_oscWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(25.0, 30.0)), module, Harm_osc::VCA1_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(45.0, 30.0)), module, Harm_osc::VCA2_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(65.0, 30.0)), module, Harm_osc::VCA3_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(85.0, 30.0)), module, Harm_osc::VCA4_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(105.0, 30.0)), module, Harm_osc::VCA5_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(125.0, 30.0)), module, Harm_osc::VCA6_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(145.0, 30.0)), module, Harm_osc::VCA7_PARAM));
-		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(165.0, 30.0)), module, Harm_osc::VCA8_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(14.0, 59.0)), module, Harm_osc::FREQ_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(28.0, 73.0)), module, Harm_osc::VCA1_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(43.0, 73.0)), module, Harm_osc::VCA2_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(58.0, 73.0)), module, Harm_osc::VCA3_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(73.0, 73.0)), module, Harm_osc::VCA4_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(88.0, 73.0)), module, Harm_osc::VCA5_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(103.0, 73.0)), module, Harm_osc::VCA6_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(118.0, 73.0)), module, Harm_osc::VCA7_PARAM));
+		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(133.0, 73.0)), module, Harm_osc::VCA8_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(13.0, 73.0)), module, Harm_osc::FREQ_PARAM));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(14.0, 79.0)), module, Harm_osc::VOCT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(29.0, 94.0)), module, Harm_osc::CV1_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(49.0, 94.0)), module, Harm_osc::CV2_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(69.0, 94.0)), module, Harm_osc::CV3_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(89.0, 94.0)), module, Harm_osc::CV4_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(109.0, 94.0)), module, Harm_osc::CV5_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(129.0, 94.0)), module, Harm_osc::CV6_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(149.0, 94.0)), module, Harm_osc::CV7_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(169.0, 94.0)), module, Harm_osc::CV8_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(13.0, 88.0)), module, Harm_osc::VOCT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(28.0, 103.0)), module, Harm_osc::CV1_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(43.0, 103.0)), module, Harm_osc::CV2_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(58.0, 103.0)), module, Harm_osc::CV3_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(73.0, 103.0)), module, Harm_osc::CV4_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(88.0, 103.0)), module, Harm_osc::CV5_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(103.0, 103.0)), module, Harm_osc::CV6_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(118.0, 103.0)), module, Harm_osc::CV7_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(133.0, 103.0)), module, Harm_osc::CV8_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(169.0, 114.0)), module, Harm_osc::OUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(133.0, 118.0)), module, Harm_osc::OUT_OUTPUT));
 
 		/*		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(13.0, 73.0)), module, Harmosc::FREQ_PARAM));
 		addParam(createParamCentered<BefacoSlidePot>(mm2px(Vec(28.0, 73.0)), module, Harmosc::VCA1_PARAM));
